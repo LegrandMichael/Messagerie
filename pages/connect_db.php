6a7429aa977ed857e069c867399036cae4fc9af8
LegrandMichael <michael.c.legrand@gmail.com>
2017-09-07 14:09:34 +0200

    Add pages folder with all redirection pages inside and template

---
+++ pages/connect_db.php
@@ -0,0 +1,10 @@
+<?php
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=messagerie;charset=utf8', 'root', 'admin');
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+;?>
---
+++ pages/template/default.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+<link href="css/font-awesome.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.css">
+<link rel="stylesheet" href="../../css/style.css">
+<title>Real Estate Carcassonne Agency</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <?php include(__DIR__.'/header.php'); ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+            <?=$content?><br />
+            </div>
+        </div>
+    </div>
+<script src="https://use.fontawesome.com/07b0ce5d10.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="../../node_modules/bootstrap/dist/js/bootstrap.js"></script>
+<script src="../../node_modules/jquery/dist/jquery.js"></script>
+<script src="../../js/script.js"></script>
+</body>
+</html>
