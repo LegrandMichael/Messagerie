df085a664aaab5769f9af985330a5ad3303babae
LegrandMichael <michael.c.legrand@gmail.com>
2017-09-07 15:39:48 +0200

    Footer done

---
+++ pages/template/footer.php
@@ -0,0 +1,32 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm-10">
+            <div class"row">
+                <div class="col-sm-12">
+                    <div class="list-group list-unstyled list-inline">
+                        <div class="list-group">
+                            <div class="col-sm-4">
+                                <div>2,28 Go (15 %) utilisés sur 15 Go</div>
+                                <div><a href="#">Gérer</a></div>
+                            </div>
+                        </div>
+                        <div class="list-group">
+                            <div class="col-sm-4">
+                                <div><a href="#">Conditions d'utilisation</a> - <a href="#">Confidentialité</a></div>
+                            </div>
+                        </div>
+                        <div class="list-group">
+                            <div class="col-sm-4 text-right">
+                                <div>Heure de connexion sur le compte : <?=date("H:i");?></div>
+                                <div><a href="#">Détails</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="row text-center"> Copyright © Footer Michaël Legrand Plugin 2017. All right reserved.</div>
+    </div>
+</div>
